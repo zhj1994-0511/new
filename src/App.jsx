b09aa733e4ea6d2560f7888f2bf9bd59e8673c73
zhj <1770876760@qq.com>
2020-12-06 21:44:57 +0800

    basic

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Button} from 'antd';
+
+export default class App extends Component{
+
+  render(){
+    return (
+
+      <div>
+        <Button type="primary">阿牛</Button>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
+// Provider 组件给app 传入了store对象
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <App />,document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
